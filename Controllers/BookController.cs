76cd1bdd77d677ca92f3634f290b3e2bf99144a4
Arghadip <arghakesh99@gmail.com>
2022-12-13 19:39:52 +0530

    Library Management System

---
+++ Controllers/BookController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Library_Management_System.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblBook.ToList());
+            }
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblBook.Where(x => x.Book_Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(tblBook book)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    dbModel.tblBook.Add(book);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblBook.Where(x => x.Book_Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, tblBook book)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    dbModel.Entry(book).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblBook.Where(x => x.Book_Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    tblBook book = dbModel.tblBook.Where(x => x.Book_Id == id).FirstOrDefault();
+                    dbModel.tblBook.Remove(book);
+                    dbModel.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,118 @@
+﻿using Library_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using(lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblCustomer.ToList());
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblCustomer.Where(x => x.Customer_Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(tblCustomer customer)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    dbModel.tblCustomer.Add(customer);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblCustomer.Where(x => x.Customer_Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, tblCustomer customer)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    dbModel.Entry(customer).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (lmsProjectEntities dbModel = new lmsProjectEntities())
+            {
+                return View(dbModel.tblCustomer.Where(x => x.Customer_Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (lmsProjectEntities dbModel = new lmsProjectEntities())
+                {
+                    tblCustomer customer = dbModel.tblCustomer.Where(x => x.Customer_Id == id).FirstOrDefault();
+                    dbModel.tblCustomer.Remove(customer);
+                    dbModel.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
